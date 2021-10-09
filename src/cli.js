f90926df9aa73a59480c534111bfe3abee271bf0
monoral01 <chebot.kirill.r@gmail.com>
2021-10-09 14:46:04 +0300

    fix Jest

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
+
 export const greeting = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hi, ${name}!`);
-}
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hi, ${name}!`);
+};
