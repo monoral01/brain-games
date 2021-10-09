a6bbee18b7cbe3428e6cc2d89796c5f3f7d53d7e
monoral01 <chebot.kirill.r@gmail.com>
2021-10-09 17:34:28 +0300

    fix linter problems

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,34 +2,31 @@ import readlineSync from 'readline-sync';
 
 const getAnswer = (str) => readlineSync.question(str);
 // Функция с логикой игры
-const numParity = () => {
-  const name = getAnswer('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let rightAnswerCount = 0;
-  do {
-    // Поскольку конкретного диапазона в задании не указано,
-    // то выбираю "случайное" число от 0 до 100
-    const pseudoRandomNumber = Math.floor(Math.random() * 100);
-    console.log(`Question: ${pseudoRandomNumber}`);
-    if (pseudoRandomNumber % 2 === 0) {
-      if (getAnswer('Your answer: ') === 'yes') {
-        console.log('Correct!');
-        rightAnswerCount += 1;
-      } else {
-        console.log('It is wrong answer :c . Correct answer was \'yes\'.');
-        console.log(`Let's try again, ${name}!`);
-        rightAnswerCount = 0;
-      }
-    } else if (getAnswer('Your answer: ') === 'no') {
+const name = getAnswer('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+let rightAnswerCount = 0;
+do {
+  // Поскольку конкретного диапазона в задании не указано,
+  // то выбираю "случайное" число от 0 до 100
+  const pseudoRandomNumber = Math.floor(Math.random() * 100);
+  console.log(`Question: ${pseudoRandomNumber}`);
+  if (pseudoRandomNumber % 2 === 0) {
+    if (getAnswer('Your answer: ') === 'yes') {
       console.log('Correct!');
       rightAnswerCount += 1;
     } else {
-      console.log('It is wrong answer :c . Correct answer was \'no\'.');
+      console.log('It is wrong answer :c . Correct answer was \'yes\'.');
       console.log(`Let's try again, ${name}!`);
       rightAnswerCount = 0;
     }
-  } while (rightAnswerCount !== 3);
-  console.log(`Congratulations, ${name}!`);
-};
-numParity();
+  } else if (getAnswer('Your answer: ') === 'no') {
+    console.log('Correct!');
+    rightAnswerCount += 1;
+  } else {
+    console.log('It is wrong answer :c . Correct answer was \'no\'.');
+    console.log(`Let's try again, ${name}!`);
+    rightAnswerCount = 0;
+  }
+} while (rightAnswerCount !== 3);
+console.log(`Congratulations, ${name}!`);
